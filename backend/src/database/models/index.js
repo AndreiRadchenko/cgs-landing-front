8bfab126b9e8f531469b619938bb701ddf1e1638
mulfyx <ilyasinkin2002@yandex.kz>
2021-04-23 09:03:06 +0600

    Create Testimonial model

--- backend/src/database/models/index.js
+++ backend/src/database/models/index.js
@@ -2,10 +2,12 @@ const file = require('./file');
 const slogan = require('./slogan');
 const project = require('./project');
 const technology = require('./technology');
+const testimonial = require('./testimonial');
 
 module.exports = {
   ...file,
   ...slogan,
   ...project,
   ...technology,
+  ...testimonial,
 };
