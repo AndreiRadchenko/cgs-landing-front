8f0848c0da636c8293dc93fdafff8d0c76b591e6
mulfyx <ilyasinkin2002@yandex.kz>
2021-04-23 09:00:44 +0600

    Re-export Project model

--- backend/src/database/models/index.js
+++ backend/src/database/models/index.js
@@ -1,9 +1,11 @@
 const file = require('./file');
 const slogan = require('./slogan');
+const project = require('./project');
 const technology = require('./technology');
 
 module.exports = {
   ...file,
   ...slogan,
+  ...project,
   ...technology,
 };
