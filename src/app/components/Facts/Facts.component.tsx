96f6d2814b89a5daf7aa27a7a8d28c20bb679cc1
DaniilKuchkovCodeGeneration <dkuch.codegeneration@gmail.com>
2021-05-28 21:26:11 +0600

    update

--- src/app/components/Facts/Facts.component.tsx
+++ src/app/components/Facts/Facts.component.tsx
@@ -7,7 +7,7 @@ const Facts = ({ fact, number }) => {
     <Styled.FactContainer className="about-us__slide" key={uuidv4()}>
       <div className="titleWrapper">
         <h2>Fact #{number}</h2>
-        <img src={fact.iconUrl} alt="" width="30" height="30" />
+        <img src={fact.iconUrl} alt="" width="30" />
       </div>
       <p className="fact__title">{fact.title}</p>
       <p className="fact__text">{fact.text}</p>
