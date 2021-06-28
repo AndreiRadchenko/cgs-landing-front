b0ec427c768c087b447c45fdda700ab01d69bda3
DaniilKuchkovCodeGeneration <dkuch.codegeneration@gmail.com>
2021-06-28 22:04:42 +0600

    update

--- src/app/components/Testimonials/Testimonials.component.tsx
+++ src/app/components/Testimonials/Testimonials.component.tsx
@@ -3,18 +3,14 @@ import * as Styled from "./Testimonials.styles";
 import { v4 as uuidv4 } from "uuid";
 import Rating from "./rating/Rating";
 import ReactCountryFlag from "react-country-flag";
-import { getName } from "country-list";
+
+import countryCodes from "../../../consts/countyCodes.json";
 
 const Testimonials = ({ testimonial }) => {
-  const country = getName(testimonial?.countryCode);
-
   const flagStyle = {
     width: "100%",
     height: "100%",
   };
-
-  const testimoniaToRender =
-    country?.split(" ").length < 3 ? country : testimonial?.countryCode;
 
   return (
     <Styled.TestimonialContainer key={uuidv4()}>
@@ -22,16 +18,16 @@ const Testimonials = ({ testimonial }) => {
         <h2>{testimonial?.customerName}</h2>
       </div>
       <div className="infoWrapper">
-        {country && (
+        {testimonial.countryCode && (
           <Styled.TestimonialFlag>
             <ReactCountryFlag
-              countryCode={testimonial?.countryCode}
+              countryCode={testimonial.countryCode}
               svg
               style={flagStyle}
             />
           </Styled.TestimonialFlag>
         )}
-        <p className="testimonials__country">{testimoniaToRender}</p>
+        <p className="testimonials__country">{countryCodes[testimonial.countryCode]}</p>
       </div>
       <p className="testimonials__company">{testimonial?.companyName}</p>
       <Styled.TestimonialPosition>
