b392ca0a563cc109e6cf36beda220d5a701a15c6
DaniilKuchkovCodeGeneration <dkuch.codegeneration@gmail.com>
2021-06-30 23:17:36 +0600

    update

--- src/app/components/Facts/SliderFacts.tsx
+++ src/app/components/Facts/SliderFacts.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Carousel from "react-multi-carousel";
-import { useMediaQuery } from "react-responsive";
 import "react-multi-carousel/lib/styles.css";
 import { IFact } from "types/components";
 import { SampleNextArrow, SamplePrevArrow } from "../shared/Slider/arrows";
@@ -13,44 +12,39 @@ interface SliderProps {
 }
 
 const Slider: React.FC<SliderProps> = ({ facts }) => {
-  const isDesktop = useMediaQuery({
-    query: '(min-width: 1024px)',
-  });
-
-  if (isDesktop) {
-    return (
+  return (
+    <Styled.Container>
       <Styled.DesktopContainer>
         {facts.map((fact, index) => (
           <Facts key={fact.id} fact={fact} number={index + 1} />
         ))}
       </Styled.DesktopContainer>
-    );
-  }
-
-  return (  
-    <Styled.SliderContainer>
-      <Carousel
-        ssr={true}
-        infinite={true}
-        swipeable={true}
-        draggable={false}
-        responsive={{
-          mobile: {
-            breakpoint: { min: 0, max: Infinity },
-            items: 1,
-            slidesToSlide: 1,
-          }
-        }}
-        customLeftArrow={<SamplePrevArrow top={9} />}
-        customRightArrow={<SampleNextArrow top={9} />}
-      >
-        {facts.map((fact, index) => (
-          <Styled.SlideContainer key={fact.id}>
-            <Facts fact={fact} number={index + 1} />
-          </Styled.SlideContainer>
-        ))}
-      </Carousel>
-    </Styled.SliderContainer>
+      <Styled.MobileContainer>
+        <Styled.SliderContainer>
+          <Carousel
+            ssr={true}
+            infinite={true}
+            swipeable={true}
+            draggable={false}
+            responsive={{
+              mobile: {
+                breakpoint: { min: 0, max: Infinity },
+                items: 1,
+                slidesToSlide: 1,
+              }
+            }}
+            customLeftArrow={<SamplePrevArrow top={9} />}
+            customRightArrow={<SampleNextArrow top={9} />}
+          >
+            {facts.map((fact, index) => (
+              <Styled.SlideContainer key={fact.id}>
+                <Facts fact={fact} number={index + 1} />
+              </Styled.SlideContainer>
+            ))}
+          </Carousel>
+        </Styled.SliderContainer>
+      </Styled.MobileContainer>
+    </Styled.Container>
   );
 };
 
