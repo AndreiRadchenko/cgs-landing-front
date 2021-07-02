e00bd54d45ff80bb265086aedfd88182adc45d82
Ilya Sinkin <ilyasinkin2002@yandex.kz>
2021-07-02 21:59:30 +0600

    add HorizontalSep between similar articles

--- next/ui/src/app/components/BlogArticle/SimilarArticles/SimilarArticles.tsx
+++ next/ui/src/app/components/BlogArticle/SimilarArticles/SimilarArticles.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react";
 import Link from "next/link";
 import sanitize from "sanitize-html";
 import { SimilarArticlesProps } from "types/components";
@@ -7,23 +8,25 @@ import * as Styled from "./SimilarArticles.styles";
 const SimilarArticles: React.FC<SimilarArticlesProps> = ({ similarArticles }) => (
   <Styled.Wrapper>
     <ul>
-      {similarArticles.slice(0, 2).map((article) => {
+      {similarArticles.slice(0, 2).map((article, index, articles) => {
+        const isLast = articles[index + 1] === undefined;
+
         return (
-          <Styled.SimilarArticle key={article.id}>
-            <Link
-              href={`/blog/[id]`}
-              as={`/blog/${article.id}`}
-            >
-              <div>
-                <img src={article.imageFileUrl} alt={article.title} />
-                <h2>{article.title}</h2>
-                <Ellipsis
-                  text={sanitize(article.content, { allowedTags: [] })}
-                  maxLines={2}
-                />
-              </div>
-            </Link>
-          </Styled.SimilarArticle>
+          <Fragment key={article.id}>
+            <Styled.SimilarArticle>
+              <Link href={`/blog/[id]`} as={`/blog/${article.id}`}>
+                <div>
+                  <img src={article.imageFileUrl} alt={article.title} />
+                  <h2>{article.title}</h2>
+                  <Ellipsis
+                    text={sanitize(article.content, { allowedTags: [] })}
+                    maxLines={2}
+                  />
+                </div>
+              </Link>
+            </Styled.SimilarArticle>
+            {!isLast && <Styled.HorizontalSep />}
+          </Fragment>
         );
       })}
     </ul>
