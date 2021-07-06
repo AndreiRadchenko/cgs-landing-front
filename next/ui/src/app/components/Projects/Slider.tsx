ea68e4a962a459a14d7bedda9e9770d4e5ac512a
Ilya Sinkin <ilyasinkin2002@yandex.kz>
2021-07-07 02:33:18 +0600

    Use new slider on the Projects

--- next/ui/src/app/components/Projects/Slider.tsx
+++ next/ui/src/app/components/Projects/Slider.tsx
@@ -1,55 +1,20 @@
-import "swiper/swiper.min.css";
-
-import React, { useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Swiper as SwiperCore } from "swiper/core";
-
-import { slides } from "../../../app/img";
-
 import { IProject } from "types/components";
 
-import { Project } from "./Project";
+import { Slider } from "../Slider";
 
-import * as Styled from "./Slider.styles";
+import { Project } from "./Project";
 
 export interface ProjectsSliderProps {
   projects: IProject[];
 }
 
 export const ProjectsSlider: React.FC<ProjectsSliderProps> = ({ projects }) => {
-  const [swiper, setSwiper] = useState<SwiperCore | null>(null);
-
-  const onPrevSlide = () => {
-    swiper?.slidePrev();
-  };
-
-  const onNextSlide = () => {
-    swiper?.slideNext();
-  };
-
   return (
-    <Styled.Container>
-      <div onClick={onPrevSlide} style={{ paddingRight: '20px' }}>
-        <img src={slides.LeftVector} alt="prev" />
-      </div>
-      <Swiper
-        loop={true}
-        slidesPerView={1}
-        onSwiper={setSwiper}
-        preloadImages={false}
-        lazy={true}
-      >
-        {projects.map((project) => (
-          <SwiperSlide key={project.id}>
-            <Styled.SlideContainer key={project.id}>
-              <Project project={project} />
-            </Styled.SlideContainer>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      <div onClick={onNextSlide} style={{ paddingLeft: '20px' }}>
-        <img src={slides.RightVector} alt="next" />
-      </div>
-    </Styled.Container>
+    <Slider
+      items={projects}
+      renderItem={(project) => (
+        <Project project={project} />
+      )}
+    />
   );
 };
