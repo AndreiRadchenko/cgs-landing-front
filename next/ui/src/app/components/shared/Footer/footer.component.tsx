043f609048fecf466a1b1f2e3e05a6921eb2d504
Andrii <yojuks@gmail.com>
2021-07-06 16:20:39 +0300

    change email in footer

--- next/ui/src/app/components/shared/Footer/footer.component.tsx
+++ next/ui/src/app/components/shared/Footer/footer.component.tsx
@@ -9,8 +9,8 @@ const Footer: React.FC<IFooter> = () => (
   <Styled.Wrapper>
     <Styled.DropBoxFooter className="drop-box-footer" data-scroll={"Social"}>
       <p className="drop-box-footer-text">
-        <a href="mailto:sales.codegeneration@gmail.com">
-          sales.codegeneration@gmail.com
+        <a href="mailto:contact@codegenerationsoftware.com">
+          contact@codegenerationsoftware.com
         </a>
       </p>
       <Styled.FooterAct>
