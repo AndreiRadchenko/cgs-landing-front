6c25e90abc7219ea25cc30da586798cfab61dc97
Ilya Sinkin <ilyasinkin2002@yandex.kz>
2021-07-07 19:23:11 +0600

    Dont reset form

--- next/ui/src/app/components/Admin/LoginForm/Form/LoginForm.tsx
+++ next/ui/src/app/components/Admin/LoginForm/Form/LoginForm.tsx
@@ -6,99 +6,97 @@ import Input from "../Input/input.commponent";
 import Button from "../Button/button.component";
 import {login} from "../../../../../services/api/adminApi";
 import Router from 'next/router'
-import {useLocalStorageState} from "use-local-storage-state";
+import { useLocalStorageState } from "use-local-storage-state";
 
 const LoginForm = () => {
-    const [isSubmitted, setSubmitted] = useState(false);
-    const [token, setToken] = useLocalStorageState("token", "");
-    const [error, setError] = useState("")
+  const [error, setError] = useState("");
+  const [token, setToken] = useLocalStorageState<string | null>("token", null);
+  const [isSubmitted, setSubmitted] = useState(false);
 
-    const isEmptyObject = (obj) => {
-        return JSON.stringify(obj) === "{}";
-    };
+  const isEmptyObject = (obj) => {
+    return JSON.stringify(obj) === "{}";
+  };
 
-    return (
-        <Formik
-            validationSchema={loginFormSchema}
-            initialValues={{
-                username: "",
-                password: "",
-            }}
+  return (
+    <Formik
+      validationSchema={loginFormSchema}
+      initialValues={{
+        username: "",
+        password: "",
+      }}
+      onSubmit={async (values) => {
+        let token: string;
 
-            onSubmit={async (values, { resetForm }) => {
-                let token: string;
+        try {
+          token = await login(values);
 
-                try {
-                    token = await login(values);
-
-                    setToken(token);
-                    setError("");
-                    resetForm({});
-                    setSubmitted(true);
-                    Router.push('/admin');
-                } catch (error) {
-                    setSubmitted(false);
-                    setError(error.response.data.error.message)
-                }                
-            }}
-        >
-            {({
-                  values,
-                  errors,
-                  touched,
-                  handleChange,
-                  handleBlur,
-                  handleSubmit,
-                  isSubmitting,
-              }) => {
-                return (
-                    <Styled.LoginWrapper>
-                    <Styled.FormWrapper>
-                        <Input
-                            placeholder="Name"
-                            type="text"
-                            name="username"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.username}
-                            errors={errors}
-                            touched={touched}
-                        />
-                        <Input
-                            placeholder="Password"
-                            type="password"
-                            name="password"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.password}
-                            errors={errors}
-                            touched={touched}
-                        />
-                        <Button
-                            type="submit"
-                            disabled={
-                                isEmptyObject(touched) || !isEmptyObject(errors) || isSubmitting
-                            }
-                            onClick={handleSubmit}
-                        >
-                            Log in
-                        </Button>
-                        {isSubmitted && (
-                            <Styled.SuccessMessageContainer>
-                                Success
-                            </Styled.SuccessMessageContainer>
-                        )}
-                        {error && (
-                            <Styled.ErrorMessage>
-                                {error}
-                            </Styled.ErrorMessage>
-                        )}
-                    </Styled.FormWrapper>
-                    </Styled.LoginWrapper>
-                );
-            }}
-        </Formik>
-    );
+          setToken(token);
+          setError("");
+          setSubmitted(true);
+          Router.push('/admin');
+        } catch (error) {
+          setError(error.response.data.error.message);
+          setSubmitted(false);
+        }        
+      }}
+    >
+      {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => {
+        return (
+          <Styled.LoginWrapper>
+          <Styled.FormWrapper>
+            <Input
+              placeholder="Name"
+              type="text"
+              name="username"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.username}
+              errors={errors}
+              touched={touched}
+            />
+            <Input
+              placeholder="Password"
+              type="password"
+              name="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+              errors={errors}
+              touched={touched}
+            />
+            <Button
+              type="submit"
+              disabled={
+                isSubmitting || isEmptyObject(touched) || !isEmptyObject(errors)
+              }
+              onClick={handleSubmit}
+            >
+              Log in
+            </Button>
+            {isSubmitted && (
+              <Styled.SuccessMessageContainer>
+                Success
+              </Styled.SuccessMessageContainer>
+            )}
+            {error && (
+              <Styled.ErrorMessage>
+                {error}
+              </Styled.ErrorMessage>
+            )}
+          </Styled.FormWrapper>
+          </Styled.LoginWrapper>
+        );
+      }}
+    </Formik>
+  );
 };
 
 export default LoginForm;
