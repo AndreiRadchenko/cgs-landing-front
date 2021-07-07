9a3a1e5c01aa55a6aaf6b308ea49e00fc246fe0d
mar1798 <m19a98r17@gmail.com>
2021-07-07 18:53:21 +0600

    change slider to new one

--- next/ui/src/app/components/FeaturedTechnologies/FeaturedTechnologies.component.tsx
+++ next/ui/src/app/components/FeaturedTechnologies/FeaturedTechnologies.component.tsx
@@ -3,27 +3,28 @@ import * as Styled from "./Featured.styles";
 import { v4 as uuidv4 } from "uuid";
 
 const Featured = ({ featuredTech }) => (
-  <Styled.FeaturedContainer key={uuidv4()}>
-    <Styled.TextContainer>
-      <h2>{featuredTech?.name}</h2>
-      <p className="featured__description">{featuredTech?.text.split('\n').map((item, idx) => (
-        <span key={idx}>
-          {item}
-          <br />
-        </span>
-      ))}</p>
-    </Styled.TextContainer>
-    <Styled.ImageContainer>
-      <img
-        className="featured__img"
-        src={featuredTech?.imageUrl}
-        alt={featuredTech?.name}
-        width="180px"
-        height="auto"
-      />
-      <p className="featured__name">{featuredTech?.label}</p>
-    </Styled.ImageContainer>
-  </Styled.FeaturedContainer>
+
+      <Styled.FeaturedContainer key={uuidv4()}>
+        <Styled.TextContainer>
+          <h2>{featuredTech?.name}</h2>
+          <p className="featured__description">{featuredTech?.text.split('\n').map((item, idx) => (
+            <span key={idx}>
+              {item}
+              <br />
+            </span>
+          ))}</p>
+        </Styled.TextContainer>
+        <Styled.ImageContainer>
+          <img
+            className="featured__img"
+            src={featuredTech?.imageUrl}
+            alt={featuredTech?.name}
+            width="180px"
+            height="auto"
+          />
+          <p className="featured__name">{featuredTech?.label}</p>
+        </Styled.ImageContainer>
+      </Styled.FeaturedContainer>
 );
 
 export default Featured;
