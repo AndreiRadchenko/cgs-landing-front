318c3c4ccf94b34f3f784a5ed551f21ccea2ed9c
Ilya Sinkin <ilyasinkin2002@yandex.kz>
2021-07-08 07:31:58 +0600

    Add loading spinner (in progress)

--- next/ui/pages/index.tsx
+++ next/ui/pages/index.tsx
@@ -1,3 +1,7 @@
+import { useState, useEffect } from "react";
+
+import styled, { createGlobalStyle } from "styled-components";
+
 import { HomePage } from "../src/app/containers/HomePage/";
 import { getData } from "../src/services/api/api";
 
@@ -28,8 +32,114 @@ export const getServerSideProps = async () => {
   }
 };
 
+const GlobalSpinnerStyle = createGlobalStyle`
+  body {
+    display: block;
+    overflow: hidden;
+  }
+
+  #__next {
+    overflow: hidden;
+  }
+`;
+
+const SpinnerContainer = styled("div")`
+  display: flex;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+  z-index: 1000;
+  background-color: white;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+`;
+
+const AnimationSpinner = styled("div")`
+  animation: spin 1s infinite linear;
+  border: solid 2vmin transparent;
+  border-radius: 50%;
+  border-right-color: #09f;
+  border-top-color: #09f;
+  box-sizing: border-box;
+  height: 20vmin;
+  left: calc(50% - 10vmin);
+  position: fixed;
+  top: calc(50% - 10vmin);
+  width: 20vmin;
+  z-index: 1;
+
+  &:before {
+    animation: spin 2s infinite linear;
+    border: solid 2vmin transparent;
+    border-radius: 50%;
+    border-right-color: #3cf;
+    border-top-color: #3cf;
+    box-sizing: border-box;
+    content: "";
+    height: 16vmin;
+    left: 0;
+    position: absolute;
+    top: 0;
+    width: 16vmin;
+  }
+
+  &:after {
+    animation: spin 3s infinite linear;
+    border: solid 2vmin transparent;
+    border-radius: 50%;
+    border-right-color: #6ff;
+    border-top-color: #6ff;
+    box-sizing: border-box;
+    content: "";
+    height: 12vmin;
+    left: 2vmin;
+    position: absolute;
+    top: 2vmin;
+    width: 12vmin;
+  }
+
+  @keyframes spin {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const Spinner = () => {
+  return (
+    <SpinnerContainer>
+      <AnimationSpinner />
+    </SpinnerContainer>
+  );
+};
+
 const Home = (props) => {
-  return <HomePage props={props} />;
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const onLoad = () => {
+      console.log("loaded");
+
+      setIsLoaded(true);
+    };
+
+    window.onload = onLoad;
+  }, []);
+
+  return (
+    <>
+      {isLoaded || (
+        <>
+          <GlobalSpinnerStyle />
+          <Spinner />
+        </>
+      )}
+      <HomePage props={props} />
+    </>
+  );
 };
 
 export default Home;
