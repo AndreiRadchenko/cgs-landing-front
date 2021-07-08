17ec53f58421f9332a502a379035ed8d120f7d5a
Andrii <yojuks@gmail.com>
2021-07-08 11:19:41 +0300

    add spiner

--- next/ui/pages/index.tsx
+++ next/ui/pages/index.tsx
@@ -57,56 +57,32 @@ const SpinnerContainer = styled("div")`
   left: 0;
 `;
 
+
 const AnimationSpinner = styled("div")`
-  animation: spin 1s infinite linear;
-  border: solid 2vmin transparent;
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+
+&:after {
+  content: " ";
+  display: block;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
   border-radius: 50%;
-  border-right-color: #09f;
-  border-top-color: #09f;
-  box-sizing: border-box;
-  height: 20vmin;
-  left: calc(50% - 10vmin);
-  position: fixed;
-  top: calc(50% - 10vmin);
-  width: 20vmin;
-  z-index: 1;
-
-  &:before {
-    animation: spin 2s infinite linear;
-    border: solid 2vmin transparent;
-    border-radius: 50%;
-    border-right-color: #3cf;
-    border-top-color: #3cf;
-    box-sizing: border-box;
-    content: "";
-    height: 16vmin;
-    left: 0;
-    position: absolute;
-    top: 0;
-    width: 16vmin;
+  border: 6px solid #113;
+  border-color: #113 transparent #113 transparent;
+  animation: spin 1.2s linear infinite;
+}
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
   }
-
-  &:after {
-    animation: spin 3s infinite linear;
-    border: solid 2vmin transparent;
-    border-radius: 50%;
-    border-right-color: #6ff;
-    border-top-color: #6ff;
-    box-sizing: border-box;
-    content: "";
-    height: 12vmin;
-    left: 2vmin;
-    position: absolute;
-    top: 2vmin;
-    width: 12vmin;
+  100% {
+    transform: rotate(360deg);
   }
-
-  @keyframes spin {
-    100% {
-      transform: rotate(360deg);
-    }
-  }
-`;
+}
+`
 
 const Spinner = () => {
   return (
@@ -121,8 +97,6 @@ const Home = (props) => {
 
   useEffect(() => {
     const onLoad = () => {
-      console.log("loaded");
-
       setIsLoaded(true);
     };
 
