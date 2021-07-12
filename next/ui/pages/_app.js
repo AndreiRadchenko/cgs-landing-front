e325145839e103dedb96c49a5ff0aab8f81ff290
Ilya Sinkin <ilyasinkin2002@yandex.kz>
2021-07-12 21:53:07 +0600

    Redirect https

--- next/ui/pages/_app.js
+++ next/ui/pages/_app.js
@@ -1,41 +1,17 @@
-import { useLayoutEffect } from "react";
-
-import Router from "next/router";
+import { ThemeProvider } from "styled-components";
 
 import "../styles/globals.css";
-import { ThemeProvider } from "styled-components";
+
 import theme from "../styles/theme";
 import { GlobalStyle } from "../styles/global-styles";
 
-const domains = new Set([
-  "codegeneration.cc",
-]);
+const App = ({ Component, pageProps }) => (
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>
+);
 
-const MyApp = ({ Component, pageProps }) => {
-  useLayoutEffect(() => {
-    if (!domains.has(window.location.host)) {
-      return;
-    }
-
-    if (window.location.protocol !== "http:") {
-      return;
-    }
-
-    const newUrl = new URL(window.location);
-
-    newUrl.protocol = "https:";
-
-    Router.push(newUrl);
-  }, []);
-
-  return (
-    <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
-  );
-};
-
-export default MyApp;
+export default App;
