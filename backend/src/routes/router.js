a919b7169e49a1e484c9adba994a1db77b00fb9e
Ilya Sinkin <ilyasinkin2002@yandex.kz>
2021-07-13 17:40:58 +0600

    Create GET /partners/get-steps

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -3,7 +3,9 @@ const Router = require('@koa/router');
 const { authRouter } = require('./auth');
 const { blogRouter } = require('./blog');
 const { homeRouter } = require('./home');
+const { partnersRouter } = require('./partners');
 
+const { stepRouter } = require('./step');
 const { factRouter } = require('./fact');
 const { fileRouter } = require('./file');
 const { sloganRouter } = require('./slogan');
@@ -21,9 +23,11 @@ router.use(
   authRouter.middleware(),
   blogRouter.middleware(),
   homeRouter.middleware(),
+  partnersRouter.middleware(),
 );
 
 router.use(
+  stepRouter.middleware(),
   factRouter.middleware(),
   fileRouter.middleware(),
   sloganRouter.middleware(),
