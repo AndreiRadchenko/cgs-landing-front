a823acdd397bd4f707d55ba442ac92a22ef2c1b0
Andrii <yojuks@gmail.com>
2021-07-19 10:49:57 +0300

    add favicon in partners page

--- next/ui/src/app/containers/PartnersPage/index.tsx
+++ next/ui/src/app/containers/PartnersPage/index.tsx
@@ -5,15 +5,17 @@ import MainLayout from "app/components/Layout/Layout";
 import * as Styled from "../../components/PartnersArticle/PartnerArticle.styles";
 import { GlobalNoUserSelectStyle } from "../../../../styles/global-no-user-select";
 import StepsSlider from "app/components/Steps/SliderSteps";
-import Interest from "app/components/Interests/Interests";
+import StepToEarn from "app/components/StepToEarn/StepToEarn";
 
-export const PartnersPage = ({ steps }) => {
+export const PartnersPage = ({ steps, stepsToEarn }) => {
+  console.log(stepsToEarn);
   return (
     <>
       <GlobalNoUserSelectStyle />
       <MainLayout
-        title="Code Generation Software | Landing"
-        description="Let's bring your idea into life together"
+        title="Your 2000$ are near you!"
+        description="Become our partner and earn money passively from each closed deal!"
+        favicon="/illustrationTitlePartners.ico"
       >
         <div className="main-wraper">
           <Article
@@ -36,10 +38,12 @@ export const PartnersPage = ({ steps }) => {
             // desc={"Become our partner and earn money passively from each closed deal!"}
           >
             <Styled.Wrapper>
-              <Styled.Interests>
-                <Interest />
-                <Styled.ImgAfterBenefits src="/interests.jpg" />
-              </Styled.Interests>
+              <Styled.StepToEarn>
+                {stepsToEarn.map((stepToEarn, index) => (
+                  <StepToEarn key={stepToEarn.id} stepToEarn={stepToEarn} number={index + 1} />
+                ))}
+                <Styled.ImgAfterBenefits src="/steps_web.png  " />
+              </Styled.StepToEarn>
             </Styled.Wrapper>
           </Article>
           <Article id="contact" title={"Just fill up the form to start our partnership!"}>
