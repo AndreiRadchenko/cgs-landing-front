4f3bfbe558f9fe1f8c90ac81dacbdf8d44f68d24
Andrii <yojuks@gmail.com>
2021-07-20 09:47:14 +0300

    change name-list and title in admin page

--- next/ui/src/consts/lists.ts
+++ next/ui/src/consts/lists.ts
@@ -174,6 +174,6 @@ export const AdminNav = [
   { name: "Featured Technology", id: "featuredTechnology" },
   { name: "Article", id: "article" },
   { name: "Steps", id: "step" },
-  { name: "StepsToEarn", id: "stepToEarn" },
+  { name: "Steps to Earn", id: "stepToEarn" },
   { name: "Images", id: "images" },
 ];
