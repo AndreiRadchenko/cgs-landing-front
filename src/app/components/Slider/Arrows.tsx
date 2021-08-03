ee4d881bb2c0a6e229a550ecd83f7577413ad195
DaniilKuchkovCodeGeneration <dkuch.codegeneration@gmail.com>
2021-08-03 22:12:15 +0600

    update

--- src/app/components/Slider/Arrows.tsx
+++ src/app/components/Slider/Arrows.tsx
@@ -1,57 +1,72 @@
 import React, { useMemo } from "react";
+import Image from "next/image";
 
-import { slides } from "../../../app/img";
+import leftArrowSvg from "../../../../public/leftVector.svg";
+import RightArrowSvg from "../../../../public/rightVector.svg";
 
 import * as Styled from "./Arrows.styles";
 
 export interface ArrowProps {
   visible: boolean;
-  imageSrc: string;
+  image: {
+    src: string;
+    width: number;
+    height: number;
+  };
   onClick: React.MouseEventHandler<HTMLDivElement>;
 }
 
-export const Arrow: React.FC<ArrowProps> = ({
-  visible,
-  imageSrc,
-  onClick,
-}) => {
-  const onArrowClick = useMemo(() => {
-    if (!visible) {
-      return undefined;
-    }
+export const Arrow: React.FC<ArrowProps> = ({ visible, image, onClick }) => {
+  const onArrowClick = useMemo(
+    () => {
+      if (!visible) {
+        return undefined;
+      }
 
-    return onClick;
-  }, [visible, onClick]);
+      return onClick;
+    },
+    [visible, onClick],
+  );
 
   return (
-    <Styled.ArrowContainer visible={visible} onClick={onArrowClick}>
-      <img src={imageSrc} alt={imageSrc} />
+    <Styled.ArrowContainer
+      visible={visible}
+      width={image.width}
+      height={image.height}
+      onClick={onArrowClick}
+    >
+      <Image
+        src={image.src}
+        alt={image.src}
+        width={image.width}
+        height={image.height}
+      />
     </Styled.ArrowContainer>
   );
 };
 
-export interface PrevArrowProps extends Omit<ArrowProps, "imageSrc"> {}
+export interface PrevArrowProps extends Omit<ArrowProps, "image"> {}
 
 export const PrevArrow: React.FC<PrevArrowProps> = (props) => {
   return (
     <Styled.PrevArrowContainer>
       <Arrow
         visible={props.visible}
-        imageSrc={slides.LeftVector}
+        image={leftArrowSvg}
         onClick={props.onClick}
       />
     </Styled.PrevArrowContainer>
   );
 };
 
-export interface NextArrowProps extends Omit<ArrowProps, "imageSrc"> {}
+export interface NextArrowProps extends Omit<ArrowProps, "image"> {}
 
 export const NextArrow: React.FC<NextArrowProps> = (props) => {
   return (
     <Styled.NextArrowContainer>
       <Arrow
         visible={props.visible}
-        imageSrc={slides.RightVector}
+        image={RightArrowSvg}
         onClick={props.onClick}
       />
     </Styled.NextArrowContainer>
