5ebe874c5b2babb58b0fe72c5c223c603f7f9ba7
Эржигит Имамов <erjigit17@gmail.com>
2021-10-11 12:48:48 +0600

    add compressed images, and tune config file

--- next/ui/next.config.js
+++ next/ui/next.config.js
@@ -6,7 +6,11 @@ const config = withPlaiceholder({
     ignoreDuringBuilds: true,
   },
   images: {
-    domains: ["code-generation-landing-bucket.s3.amazonaws.com"],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    domains: ["code-generation-landing-bucket.s3.amazonaws.com", "cdnjs.cloudflare.com"],
+    path: "/_next/image",
+    logger: 'default',
   },
 });
 
