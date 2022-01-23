501a81b29125cf9b320ea6866f8af5f9d72c4496
Hibanaga <vladyslav.tykhoniuk.works@gmail.com>
2022-01-23 21:00:43 +0100

    Remove from homepage component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,12 @@ import React from "react";
 import type { NextPage } from "next";
 import Body from "../components/Body/Body";
 import * as StyledCommon from "../styles/Page.styled";
-import Partners from "../components/Partners/Partners";
-
 
 const Home: NextPage = () => {
   return (
     <div>
       <StyledCommon.Page>
         <Body />
-      <Partners />
       </StyledCommon.Page>
     </div>
   );
