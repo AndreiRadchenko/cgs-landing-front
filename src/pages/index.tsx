2dd7974ac55a8da690bc68f91ded375cb16f74c4
Hibanaga <vladyslav.tykhoniuk.works@gmail.com>
2022-01-23 20:59:37 +0100

    Create Partners section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import type { NextPage } from "next";
 import Body from "../components/Body/Body";
 import * as StyledCommon from "../styles/Page.styled";
+import Partners from "../components/Partners/Partners";
+
 
 const Home: NextPage = () => {
   return (
     <div>
       <StyledCommon.Page>
         <Body />
+      <Partners />
       </StyledCommon.Page>
     </div>
   );
