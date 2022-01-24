7c0804da5e0775b16baa1163b3f6315d3149f812
Hibanaga <vladyslav.tykhoniuk.works@gmail.com>
2022-01-24 12:43:31 +0100

    Resolve themes conflicts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import type { NextPage } from "next";
 import Body from "../components/Body/Body";
 import * as StyledCommon from "../styles/Page.styled";
-import AboutUs from "../components/AboutUs/AboutUs";
 
 const Home: NextPage = () => {
   return (
     <div>
       <StyledCommon.Page>
         <Body />
-        <AboutUs />
       </StyledCommon.Page>
     </div>
   );
