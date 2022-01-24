985d445babbccf2b64a4cea1bf608724dbc3d756
Hibanaga <vladyslav.tykhoniuk.works@gmail.com>
2022-01-24 16:27:32 +0100

    Create component our team

---
+++ src/components/OurTeam/OurTeam.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as StyledThisComp from "../../styles/OurTeam.styled";
+
+const OurTeam = () => {
+  return <></>;
+};
+
+export default OurTeam;
---
+++ src/styles/OurTeam.styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const OurTeamContainer = styled.section`
+  width: 1400px;
+  margin: 0 auto;
+`;
+
+export const OurTeamRow = styled.div`
+  display: flex;
+`;
