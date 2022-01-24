5d585de4f55815faa2755b6370b3ffdd6003915c
Hibanaga <vladyslav.tykhoniuk.works@gmail.com>
2022-01-24 11:04:04 +0100

    Temporary delete component Project from home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,12 @@ import React from "react";
 import type { NextPage } from "next";
 import Body from "../components/Body/Body";
 import * as StyledCommon from "../styles/Page.styled";
-import Projects from "../components/Projects/Projects";
-
 
 const Home: NextPage = () => {
   return (
     <div>
       <StyledCommon.Page>
-        {/*<Body />*/}
-      <Projects />
+        <Body />
       </StyledCommon.Page>
     </div>
   );
