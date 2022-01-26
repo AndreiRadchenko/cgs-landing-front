072057dd7c3785748a178f3b5c74aec6eb078114
vitalykostak <vitaliy.k@codegenerationsoftware.com>
2022-01-26 13:07:11 +0100

    Implement mobile responsive layout for YesBegin block

--- src/components/YesBegin/YesBegin.styled.ts
+++ src/components/YesBegin/YesBegin.styled.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import themes from "../../utils/themes";
 
 export const Container = styled.div`
-  height: 100vh;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -11,15 +11,35 @@ export const Container = styled.div`
 `;
 
 export const Title = styled.h2`
+  margin: 0;
   font-size: 5em;
   text-align: center;
-  margin: 0;
+  max-width: 10em;
+
+  @media ${themes.media.maxTabletPortrait} {
+    font-size: 4em;
+  }
+
+  @media ${themes.media.maxMobile} {
+    font-size: 3em;
+    max-width: 11em;
+  }
 `;
 
 export const SubTitle = styled.h4`
+  margin: 1em 0 0 0;
   font-size: 3em;
   text-align: center;
-  margin: 1em 0 0 0;
+  font-weight: ${themes.primary.font.weight.light};
+
+  @media ${themes.media.maxTabletPortrait} {
+    font-size: 2.5em;
+  }
+
+  @media ${themes.media.maxMobile} {
+    font-size: 2.2em;
+    max-width: 14em;
+  }
 `;
 
 export const ButtonContainer = styled.div`
@@ -33,4 +53,18 @@ export const ImageContainer = styled.div`
   right: -4em;
   bottom: -1.9em;
   z-index: -1;
+
+  @media ${themes.media.maxTabletPortrait} {
+    width: 11em;
+    height: 12.2em;
+    right: -6em;
+    bottom: -4.9em;
+  }
+
+  @media ${themes.media.maxMobile} {
+    width: 13em;
+    height: 14.2em;
+    right: -4em;
+    bottom: -8.9em;
+  }
 `;
