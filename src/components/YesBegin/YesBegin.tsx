b5b1391f37a32fadf42947929b2bfe2077a7dba2
vitalykostak <vitaliy.k@codegenerationsoftware.com>
2022-01-27 17:20:10 +0100

    Add scroll to form by buttons in YesBegin and WelcomePage

--- src/components/YesBegin/YesBegin.tsx
+++ src/components/YesBegin/YesBegin.tsx
@@ -3,8 +3,10 @@ import * as Styles from "./YesBegin.styled";
 import BaseButton from "../BaseButton/BaseButton";
 import ButtonTextWrapper from "../ButtonText/ButtonTextWrapper";
 import Image from "next/image";
+import { IYesBeginProps } from "./types";
 
-const YesBegin = () => {
+const YesBegin = ({ clickHandler }: IYesBeginProps) => {
+
   return (
     <Styles.Container>
       <Styles.Title>
@@ -15,7 +17,7 @@ const YesBegin = () => {
         Do you want to build a rocket with CGS-team?
       </Styles.SubTitle>
       <Styles.ButtonContainer>
-        <BaseButton>
+        <BaseButton onClick={clickHandler}>
           <ButtonTextWrapper fontSize="2em">yes! begin!</ButtonTextWrapper>
         </BaseButton>
         <Styles.ImageContainer>
