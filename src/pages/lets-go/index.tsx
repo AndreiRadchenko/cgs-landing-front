7c689c87fbfd66c09a076bc0eb4a191212e5559d
baliubakh <sanya0000010@gmail.com>
2022-04-06 08:43:39 +0300

    feat: Added footer to the page

--- src/pages/lets-go/index.tsx
+++ src/pages/lets-go/index.tsx
@@ -2,12 +2,16 @@
 
 import * as StyledCommon from "../../styles/Page.styled";
 import WhatWeLikeToSee from "../../components/WhatWeLikeToSee/WhatWeLikeToSee";
+import Footer from "../../components/Footer/Footer";
 
 const letsGo = () => {
   return (
-    <StyledCommon.Page>
-      <WhatWeLikeToSee />
-    </StyledCommon.Page>
+    <>
+      <StyledCommon.Page>
+        <WhatWeLikeToSee />
+      </StyledCommon.Page>
+      <Footer />
+    </>
   );
 };
 
