9bb8d22088d59c437ba645b736f568795b59bce8
baliubakh <sanya0000010@gmail.com>
2022-04-14 10:05:16 +0300

    fix: Fixed width and height for mobile

--- src/components/CreateSupportForm/CreateSupportForm.styled.ts
+++ src/components/CreateSupportForm/CreateSupportForm.styled.ts
@@ -19,7 +19,11 @@ export const Title = styled.h2`
   margin: 0 0 1em 0;
 
   @media ${themes.primary.media.maxTabletLandScape} {
+    margin: 0 0 0.2em 0;
     font-size: 3.8em;
+  }
+  @media ${themes.primary.media.onlyTabletPortrait} {
+    margin: 0 0 0.3em 0;
   }
 
   @media ${themes.primary.media.maxMobile} {
@@ -32,4 +36,21 @@ export const ContentWrapper = styled.div``;
 export const ButtonRow = styled.div`
   display: flex;
   justify-content: center;
+
+  @media ${themes.primary.media.onlyTabletLandScape} {
+    button {
+      /* background-color: red; */
+      width: 12em;
+      height: 5em;
+      margin-top: 0;
+    }
+  }
+  @media ${themes.primary.media.onlyTabletPortrait} {
+    button {
+      /* background-color: red; */
+      width: 12em;
+      height: 5em;
+      margin-top: 0;
+    }
+  }
 `;
