843732750da76054c2f598c737142c58fbb91b41
Timyr4ikon <nbvehnbvehnbve@gmail.com>
2022-04-17 15:31:17 +0300

    Implement services

---
+++ src/services/example/index.ts
@@ -0,0 +1,28 @@
+import {HttpService} from '../../common/services/http.service';
+import {HttpServiceFactory} from '../../common/services/index';
+
+export class ExampleService {
+  constructor(private httpService: HttpService) {}
+  public examplePost(email: string, password: string) {
+    return this.httpService.post<IResponse, IData>('example/post', {
+      email,
+      password,
+    });
+  }
+}
+
+const factory = new HttpServiceFactory();
+export const exampleService = new ExampleService(factory.createHttpService());
+
+//how to use in your component with react-query
+const addExampleMutation = useMutation(
+  ({email, password}: {email: string; password: string}) =>
+    exampleService.examplePost(email, password),
+  {
+    onSuccess: () => {
+      queryClient.invalidateQueries('get_data');
+    },
+  },
+);
+
+//code services in a component folder
