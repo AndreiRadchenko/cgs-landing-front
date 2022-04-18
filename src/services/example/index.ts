2f9dbb1dfd1c67748e5341449e844676c32ecccc
Just-Riko <kitchakpavlonew@gmail.com>
2022-04-18 12:14:02 +0300

    fix: Sync with master

--- src/services/example/index.ts
+++ src/services/example/index.ts
@@ -2,6 +2,10 @@ import { IResponse } from '../../types/Admin';
 import {HttpService} from '../http.service';
 import {HttpServiceFactory} from '../index';
 
+interface IData {
+  // your interface
+}
+
 export class ExampleService {
   constructor(private httpService: HttpService) {}
   public examplePost(email: string, password: string) {
@@ -16,14 +20,15 @@ const factory = new HttpServiceFactory();
 export const exampleService = new ExampleService(factory.createHttpService());
 
 //how to use in your component with react-query
-const addExampleMutation = useMutation(
-  ({email, password}: {email: string; password: string}) =>
-    exampleService.examplePost(email, password),
-  {
-    onSuccess: () => {
-      queryClient.invalidateQueries('get_data');
-    },
-  },
-);
+
+// const addExampleMutation = useMutation(
+//   ({email, password}: {email: string; password: string}) =>
+//     exampleService.examplePost(email, password),
+//   {
+//     onSuccess: () => {
+//       queryClient.invalidateQueries('get_data');
+//     },
+//   },
+// );
 
 //code services in a component folder
