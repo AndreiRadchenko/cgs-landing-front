ed0f4027e05909205d9d7924b68ef8cee88814e5
Maksym Dzhafarov <maksim8797@gmail.com>
2022-04-26 20:06:54 +0400

    fix: animation slide howWeWorkList

--- src/components/HowWeWorkList/HowWeWorkList.tsx
+++ src/components/HowWeWorkList/HowWeWorkList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Mousewheel, Pagination } from "swiper";
 import "swiper/css";
@@ -6,19 +6,41 @@ import "swiper/css/mousewheel";
 
 import HowWeWorkCard from "../HowWeWorkCard/HowWeWorkCard";
 import { IHowWeWorkCardProps } from "../HowWeWorkCard/types";
+import useIntersectionObserver from "../../hooks/useIntersectionObserver";
+import * as Styles from "./HowWeWorkList.styled";
 
 SwiperCore.use([Mousewheel, Pagination]);
 
 const HowWeWorkList = ({ items }: { items: IHowWeWorkCardProps[] }) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const ref = useRef<HTMLDivElement | null>(null);
+  const entry = useIntersectionObserver(ref, { threshold: 0.75 });
+  const isVisible = !!entry?.isIntersecting;
+
   return (
-    <Swiper spaceBetween={10} mousewheel={{ releaseOnEdges: true }}>
-      {items &&
-        [...items].map((item, idx) => (
-          <SwiperSlide key={idx}>
-            <HowWeWorkCard key={idx.toString()} {...item} />
-          </SwiperSlide>
-        ))}
-    </Swiper>
+    <Styles.Container ref={ref}>
+      {!isVisible ? (
+        <HowWeWorkCard {...items[currentSlide]} />
+      ) : (
+        <Swiper
+          observer={true}
+          spaceBetween={10}
+          mousewheel={{ releaseOnEdges: true }}
+          speed={700}
+          pagination={{ clickable: true }}
+          onSlideChange={(e) => setCurrentSlide(e.activeIndex)}
+          initialSlide={currentSlide}
+        >
+          {items &&
+            [...items].map((item, idx) => (
+              <SwiperSlide key={idx}>
+                <HowWeWorkCard key={idx.toString()} {...item} />
+              </SwiperSlide>
+            ))}
+        </Swiper>
+      )}
+    </Styles.Container>
   );
 };
 
