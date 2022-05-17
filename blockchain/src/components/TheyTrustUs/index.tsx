3e95c8b2df52383b2eb924181eb6713e15f2856f
Just-Riko <kitchakpavlonew@gmail.com>
2022-05-17 17:14:17 +0300

    feature: added some images

--- blockchain/src/components/TheyTrustUs/index.tsx
+++ blockchain/src/components/TheyTrustUs/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import * as UsefullStyled from "../../styles/usefullStyled";
 import * as Styled from "../../styles/theyTrustUs";
 import Image from "next/image";
@@ -6,6 +6,10 @@ import ufo from "../../../public/ufo.png";
 import { logos } from "../../utils/logos";
 
 const TheyTrustUs = () => {
+  const [isHowered, setIsHowered] = useState(false);
+  const mouseOn = () => setIsHowered(true);
+  const mouseLeave = () => setIsHowered(false);
+
   return (
     <UsefullStyled.TitlePlusContentBlock>
       <div>
@@ -14,10 +18,10 @@ const TheyTrustUs = () => {
           <Image src={ufo} />
         </Styled.UfoBlock>
       </div>
-      <Styled.LogosGrid>
+      <Styled.LogosGrid onMouseOver={mouseOn} onMouseLeave={mouseLeave}>
         {logos.map((i, ind) => (
           <div key={`theyLogoInd${ind}`}>
-            <Image src={i} />
+            <Image src={isHowered ? i.green : i.image} key={`theyLogoInd${ind}`} />
           </div>
         ))}
       </Styled.LogosGrid>
