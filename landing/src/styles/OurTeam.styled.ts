a4ce74506a67b04570cda5cdec55dee1dc57cc13
baliubakh <74013378+baliubakh@users.noreply.github.com>
2022-05-24 13:58:59 +0300

    fix: fixed project categories

--- landing/src/styles/OurTeam.styled.ts
+++ landing/src/styles/OurTeam.styled.ts
@@ -1,4 +1,4 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import * as animations from "./AnimationsStyled";
 import themes from "../utils/themes";
 import Decoration from "../components/Decoration/Decoration";
