eb55b9a0cd028bf0fe4ffbf85156373484af94ce
baliubakh <74013378+baliubakh@users.noreply.github.com>
2022-06-01 12:26:18 +0300

    feat: added schema org script

--- landing/src/pages/careers/index.tsx
+++ landing/src/pages/careers/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import Careers from "../components/Careers";
-import CareersForm from "../components/CareersForm/index";
-import { Page } from "../styles/Page.styled";
+import Careers from "../../components/Careers";
+import CareersForm from "../../components/CareersForm/index";
+import { Page } from "../../styles/Page.styled";
 import { NextPage } from "next";
-import HeaderNav from "../components/HeaderNav/HeaderNav";
-import Footer from "../components/Footer/Footer";
+import HeaderNav from "../../components/HeaderNav/HeaderNav";
+import Footer from "../../components/Footer/Footer";
 import { useQuery } from "react-query";
-import { queryKeys } from "../consts/queryKeys";
-import { adminCareersService } from "../services/adminCareersPage";
-import { CareersProps } from "../types/Admin/Admin.types";
+import { queryKeys } from "../../consts/queryKeys";
+import { adminCareersService } from "../../services/adminCareersPage";
+import { CareersProps } from "../../types/Admin/Admin.types";
 
 const CarrersPage: NextPage = () => {
   const { data, isLoading }: CareersProps = useQuery(
