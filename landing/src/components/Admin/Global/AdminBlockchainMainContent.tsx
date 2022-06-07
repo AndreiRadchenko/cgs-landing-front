4a2278f99dfa1b7d11ed35f1b23374c259bc899a
blackberry-v <yakymenko.v@cgsteam.io>
2022-06-07 14:12:54 +0300

    feature Add logo blocks to blockchain admin page

--- landing/src/components/Admin/Global/AdminBlockchainMainContent.tsx
+++ landing/src/components/Admin/Global/AdminBlockchainMainContent.tsx
@@ -1,12 +1,53 @@
 import React from "react";
+import AdminLogosBlock from "../LogosBlock";
+import { useQuery } from "react-query";
+import { queryKeys } from "../../../consts/queryKeys";
+import { adminGlobalService } from "../../../services/adminHomePage";
+import { IDataResponse } from "../../../types/Admin/Response.types";
+import { Form, Formik } from "formik";
+import * as Styled from "../../../styles/AdminPage";
+
+interface IMainProps {
+  data: IDataResponse | undefined;
+  isLoading: boolean;
+  refetch: () => Promise<IMainProps>;
+}
 
 const AdminBlockchainMainContent = () => {
-  return (
-    <>
-      {/*
-	components here
-	*/}
-    </>
+  const { data, isLoading, refetch }: IMainProps = useQuery(
+    queryKeys.GetFullPage,
+    () => adminGlobalService.getFullPage()
+  );
+
+  const submitForm = async () => {
+    document.body.style.cursor = "wait";
+    // await mutateAsync(values);
+    await refetch();
+    document.body.style.cursor = "auto";
+  };
+  return isLoading ? (
+    <Styled.AdminUnauthorizedModal>Loading...</Styled.AdminUnauthorizedModal>
+  ) : data !== undefined ? (
+    <Formik
+      initialValues={data!}
+      onSubmit={submitForm}
+      validateOnChange={false}
+    >
+      {() => {
+        return (
+          <Styled.AdminContentBlock>
+            <Form>
+              <AdminLogosBlock typeOfLogo={"normal"} />
+              <AdminLogosBlock typeOfLogo={"hover"} />
+            </Form>
+          </Styled.AdminContentBlock>
+        );
+      }}
+    </Formik>
+  ) : (
+    <Styled.AdminUnauthorizedModal>
+      Something went wrong :(
+    </Styled.AdminUnauthorizedModal>
   );
 };
 
