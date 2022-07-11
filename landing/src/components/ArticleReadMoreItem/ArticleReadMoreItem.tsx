08eaea971248f469a72189afc68f1a58cf617c7d
blackberry-v <yakymenko.v@cgsteam.io>
2022-07-11 22:41:09 +0300

    fix: read more section

--- landing/src/components/ArticleReadMoreItem/ArticleReadMoreItem.tsx
+++ landing/src/components/ArticleReadMoreItem/ArticleReadMoreItem.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import * as Styles from "./ArticleReadMoreItem.styled";
-import { IArticle } from "../../types/Admin/Response.types";
+import { IArticle, IView, IViews } from "../../types/Admin/Response.types";
 import Link from "next/link";
 import AdminBlackButton from "../Admin/Global/AdminBlackButton";
 import Image from "next/image";
 import viewsIcon from "../../../public/viewsIcon.svg";
 import { useRouter } from "next/router";
+import { useQuery } from "react-query";
+import { queryKeys } from "../../consts/queryKeys";
+import { adminBlogService } from "../../services/adminBlogPage";
 
 interface IArticleReadMore {
   article: IArticle;
+  view: number | undefined;
 }
 
-const ArticleReadMoreItem = ({ article }: IArticleReadMore) => {
+const ArticleReadMoreItem = ({ article, view }: IArticleReadMore) => {
   const navigation = useRouter();
 
   return (
@@ -22,15 +26,15 @@ const ArticleReadMoreItem = ({ article }: IArticleReadMore) => {
       />
       <Styles.ViewsWrapper>
         <Image src={viewsIcon} alt="views icon" />
-        <Styles.AmountViews>3081</Styles.AmountViews>
+        <Styles.AmountViews>{view}</Styles.AmountViews>
       </Styles.ViewsWrapper>
       <Styles.Title onClick={() => navigation.push(`/blog/${article.url}`)}>
         {article.title}
       </Styles.Title>
       <Styles.Description>{article.description}</Styles.Description>
-      <Link href={`/blog/${article.url}`} passHref>
-        <AdminBlackButton text="Read more" />
-      </Link>
+      <a href={`/blog/${article.url}`}>
+        <Styles.ReadMoreButton>Read more</Styles.ReadMoreButton>
+      </a>
     </Styles.Wrapper>
   );
 };
