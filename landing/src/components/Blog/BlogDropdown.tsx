294dfc5ea38a0f0874d044059e19fe8bb949d70c
sekkure <secreori@gmail.com>
2022-07-11 15:03:30 +0300

    feat: add more customizations to dropdawn

--- landing/src/components/Blog/BlogDropdown.tsx
+++ landing/src/components/Blog/BlogDropdown.tsx
@@ -1,14 +1,25 @@
 import React, { useEffect, useState } from "react";
 import * as Styled from "../../styles/Blog.styled";
-import { blogTags } from "../../utils/variables";
+import Arrow from "../../../public/upArrowSidebar.svg";
+import Image from "next/image";
 
 interface IBlogDropdown {
   setFilter: (tag: string) => void;
   filter: string | null;
   tags: string[];
+  dropdownName: string;
+  isHeader?: boolean;
+  isTag?: boolean;
 }
 
-const BlogDropdown = ({ setFilter, filter, tags }: IBlogDropdown) => {
+const BlogDropdown = ({
+  setFilter,
+  filter,
+  tags,
+  dropdownName,
+  isTag = false,
+  isHeader = false,
+}: IBlogDropdown) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   useEffect(() => {
     setIsOpen(false);
@@ -19,13 +30,15 @@ const BlogDropdown = ({ setFilter, filter, tags }: IBlogDropdown) => {
       <Styled.DropdownButton
         className={isOpen ? "open" : ""}
         onClick={() => setIsOpen(!isOpen)}
+        isHeader={isHeader}
       >
-        #TAGS
+        {dropdownName}
+        <Image src={Arrow} alt="Arrow" />
       </Styled.DropdownButton>
       <Styled.DropdownContent className={isOpen ? "open" : ""}>
         {tags.map((tag) => (
           <div onClick={() => setFilter(tag)} key={tag}>
-            {`#${tag}`}
+            {isTag ? `#${tag}` : tag}
           </div>
         ))}
       </Styled.DropdownContent>
