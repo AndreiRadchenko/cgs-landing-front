6b5500329f48a1a448b29d790661ab0c3dcae023
IvanDarmovis <ivan.darmovis1@gmail.com>
2022-08-25 17:47:18 +0300

    fix: change Provides blocks architext

--- landing/src/styles/OngoingSupport/Layout.ts
+++ landing/src/styles/OngoingSupport/Layout.ts
@@ -5,4 +5,6 @@ export const Layout = styled.div`
   margin: 0 4.25em;
   font-weight: ${themes.primary.font.weight.heavy};
   font-family: ${themes.primary.font.family.namu};
+  overflow: clip;
+  overflow-clip-margin: 50px;
 `;
--- landing/src/styles/OngoingSupport/ProvidesBlock.styled.ts
+++ landing/src/styles/OngoingSupport/ProvidesBlock.styled.ts
@@ -14,8 +14,8 @@ export const Bgi = styled.div`
   background-repeat: no-repeat;
 
   width: 107.64vw;
-  height: 39vw;
-  position: absolute;
+  height: 39.6vw;
+  position: relative;
   left: -4.3em;
   top: 19.5em;
 `;
@@ -47,30 +47,139 @@ export const Text = styled.p`
 export const TextWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-between;
-  margin-top: 5.67em;
+  column-gap: 7em;
+  position: relative;
+  top: -10em;
+  width: 90%;
+  margin: 0 auto;
+
+  @media (max-width: 1299px) {
+    column-gap: 5em;
+  }
+
+  @media (max-width: 1299px) {
+    column-gap: 3.5em;
+    width: 89%;
+    top: -11em;
+  }
+
+  @media (max-width: 1179px) {
+    top: -12em;
+    width: 87%;
+    column-gap: 2em;
+  }
+
+  & p {
+    @media (max-width: 799px) {
+      margin-top: 0;
+    }
+  }
 
   & > div {
     margin-bottom: 8.08em;
-
     flex-basis: 28.5%;
+
+    @media (max-width: 1299px) {
+      margin-bottom: 6em;
+    }
+
+    @media (max-width: 1199px) {
+      margin-bottom: 4em;
+    }
+
+    @media (max-width: 1063px) {
+      margin-bottom: 2.8em;
+    }
+
+    @media (max-width: 862px) {
+      margin-bottom: 2em;
+    }
+
+    @media (max-width: 799px) {
+      margin-bottom: 3em;
+    }
 
     &:nth-child(4) {
       margin-left: 13.5em;
+
+      @media (max-width: 1299px) {
+        margin-left: 11em;
+      }
+
+      @media (max-width: 1199px) {
+        flex-basis: 30%;
+        margin-left: 8em;
+      }
+
+      /* @media (max-width: 1179px) {
+        flex-basis: 35%;
+        margin-left: 4em;
+      } */
+
+      @media (max-width: 1171px) {
+        margin-left: 2em;
+        flex-basis: 40%;
+      }
+
+      @media (max-width: 864px) {
+        margin-left: 0em;
+        flex-basis: 45%;
+      }
     }
 
     &:nth-child(2) {
       margin-top: 7.17em;
+
+      @media (max-width: 1199px) {
+        flex-basis: 33%;
+      }
+
+      @media (max-width: 1063px) {
+        margin-top: 5em;
+      }
+
+      @media (max-width: 991px) {
+        margin-top: 8em;
+      }
+
+      @media (max-width: 938px) {
+        margin-top: 5em;
+      }
     }
 
     &:nth-child(3) {
       & > p {
         max-width: 19em;
       }
+
+      @media (max-width: 864px) {
+        flex-basis: 32%;
+        margin-left: -0.5em;
+      }
     }
 
     &:last-child {
-      margin-right: 13em;
+      margin-left: auto;
+      margin-right: 17em;
+
+      @media (max-width: 1299px) {
+        margin-right: 15em;
+      }
+
+      @media (max-width: 1199px) {
+        flex-basis: 35%;
+        margin-right: 8em;
+      }
+
+      @media (max-width: 1171px) {
+        margin-right: 4em;
+        flex-basis: 40%;
+      }
+
+      @media (max-width: 864px) {
+        margin-right: 1em;
+        flex-basis: 45%;
+      }
     }
   }
 `;
