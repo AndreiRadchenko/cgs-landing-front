862be559dcf1b4c3234e7cfaf615052fcd940aca
IvanDarmovis <ivan.darmovis1@gmail.com>
2022-08-30 18:55:43 +0300

    bug: take color from constants

--- landing/src/styles/BlockchainService/LinedText.styled.ts
+++ landing/src/styles/BlockchainService/LinedText.styled.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import themes from "../../utils/themes";
 
 export const Container = styled.div<{ position: string; height: number }>`
   display: flex;
@@ -6,7 +7,7 @@ export const Container = styled.div<{ position: string; height: number }>`
   height: ${({ height }) => `${height}px`};
   align-items: ${({ position }) =>
     position === "top" ? "flex-start" : "flex-end"};
-  border-left: 1px solid #000;
+  border-left: 1px solid ${themes.primary.colors.black};
   z-index: 5;
 `;
 
@@ -14,9 +15,9 @@ export const Marker = styled.div<{ position: string }>`
   width: 12px;
   height: 12px;
   background: #f0efed;
-  border: 1px solid #000000;
+  border: 1px solid ${themes.primary.colors.black};
   border-radius: 50%;
-  box-shadow: 7px 2px 0px #000000;
+  box-shadow: 7px 2px 0px ${themes.primary.colors.black};
   margin-left: -7px;
   transform: translateY(
     ${({ position }) => (position === "top" ? "-90%" : "90%")}
