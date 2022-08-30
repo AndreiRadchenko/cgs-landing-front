3a2d4c9d38a69911521609f26fdfdd630712ec70
IvanDarmovis <ivan.darmovis1@gmail.com>
2022-08-30 14:24:21 +0300

    bug: Remarks corrected

--- landing/src/styles/BlockchainService/Layout.ts
+++ landing/src/styles/BlockchainService/Layout.ts
@@ -5,4 +5,11 @@ export const Layout = styled.div`
   padding: 0 4.25em;
   font-family: ${themes.primary.font.family.namu};
   font-weight: ${themes.primary.font.weight.heavy};
+
+  overflow-x: hidden;
+
+  @media ${themes.primary.media.minPCFullHD} {
+    margin: auto;
+    width: 1785px;
+  }
 `;
