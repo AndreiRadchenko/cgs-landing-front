7b3566cbc1930c8cd79afb89fb0f0c14c5e5bbf0
Serhii Malyshko <sergiy.malyshko@codegenerationsoftware.com>
2022-09-09 11:44:32 +0300

    fix: fixed mobile about us

--- landing/src/components/AboutUsPage/AboutUs.styled.ts
+++ landing/src/components/AboutUsPage/AboutUs.styled.ts
@@ -46,7 +46,7 @@ export const Subtitle = styled.div`
   font-weight: ${themes.primary.font.weight.heavy};
   margin-top: 40px;
   &:first-child {
-    margin-top: 0;
+    margin-top: -30px;
   }
   @media ${themes.primary.media.maxTabletLandScape} {
     font-size: ${themes.primary.font.size.smallAboutSubtitle};
@@ -85,13 +85,13 @@ export const Text = styled.div`
 export const BottomImage = styled.img`
   position: absolute;
   width: 54.1%;
-  bottom: 80px;
+  bottom: 126px;
   right: 0;
 
   @media ${themes.primary.media.maxMobile} {
-    width: 493px;
+    width: 500px;
     transform: rotate(-4.51deg);
-    right: -55px;
+    right: -42px;
   }
 `;
 
@@ -372,7 +372,7 @@ export const RowContainer = styled.div`
     }
 
     &:nth-child(3) {
-      bottom: 400px;
+      bottom: 460px;
       left: 40px;
       & > div {
         &:nth-child(2) {
@@ -422,7 +422,8 @@ export const OurBonuses = styled.div`
   padding: 115px 0 370px;
 
   @media ${themes.primary.media.maxMobile} {
-    padding-bottom: 361px;
+    padding-top: 0;
+    padding-bottom: 376px;
   }
 `;
 
