15e67c5f4c5eeb654fa8cc86a08a8c6ad76f55d4
IvanDarmovis <ivan.darmovis1@gmail.com>
2022-09-12 12:27:25 +0300

    bug: corrections

--- landing/src/styles/DbService/HeadBlock.styled.ts
+++ landing/src/styles/DbService/HeadBlock.styled.ts
@@ -6,7 +6,7 @@ export const Container = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-between;
-  padding: 9.5em 0 0;
+  padding: 9.3em 0 0;
   position: relative;
 
   @media ${themes.primary.media.maxMobile} {
