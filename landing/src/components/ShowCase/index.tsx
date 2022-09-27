99ac424eea7c3ba6a310b3d9cf1d58c63dc309ab
baliubakh <74013378+baliubakh@users.noreply.github.com>
2022-09-27 10:42:49 +0300

    feat: basic new showcase design

--- landing/src/components/ShowCase/index.tsx
+++ landing/src/components/ShowCase/index.tsx
@@ -4,6 +4,7 @@ import { useQuery } from "react-query";
 import { queryKeys } from "../../consts/queryKeys";
 import { adminPortfolioPageService } from "../../services/adminPortfolioPage";
 import { IReviewProps } from "../../types/Admin/Response.types";
+import { Navigation } from "swiper";
 
 import { Swiper, SwiperProps, SwiperSlide } from "swiper/react";
 
@@ -13,31 +14,26 @@ import "swiper/css/pagination";
 import Slide from "./Slide";
 
 const SliderProps: SwiperProps = {
-  slidesPerView: 3.1,
-  spaceBetween: 28,
-  className: "mySwiper",
+  slidesPerView: 2,
   loop: true,
+  className: "mySwiper",
+  modules: [Navigation],
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 
   breakpoints: {
-    2420: {
-      slidesPerView: 4.1,
-    },
-    1375: {
-      slidesPerView: 3.1,
-    },
-    940: {
-      slidesPerView: 2.1,
-    },
-    768: { spaceBetween: 11, slidesPerView: 1.17 },
-    670: { spaceBetween: 11, slidesPerView: 2.05 },
-    374: {
-      spaceBetween: 11,
-      slidesPerView: 1.17,
-    },
-    300: {
-      centeredSlides: true,
-      slidesPerView: 1,
-    },
+    // 2420: {
+    //   slidesPerView: 2,
+    // },
+
+    768: { slidesPerView: 2, spaceBetween: 0 },
+    // 670: { slidesPerView: 2.05 },
+    // 374: {
+    //   slidesPerView: 1.17,
+    // },
+    300: { slidesPerView: 1, spaceBetween: 21 },
   },
 };
 
@@ -46,6 +42,8 @@ interface IShowCase {
 }
 
 const ShowCase = ({ projects }: IShowCase) => {
+  const [hoverNext, setHoverNext] = useState<boolean>(false);
+  const [hoverPrev, setHoverPrev] = useState<boolean>(false);
   const [currentProjects, setCurrentProjects] = useState<IReviewProps[]>();
   const { data } = useQuery(queryKeys.getPortfolio, () =>
     adminPortfolioPageService.getPortfolio()
@@ -57,16 +55,36 @@ const ShowCase = ({ projects }: IShowCase) => {
     );
   }, [data?.reviews, projects]);
 
+  const handleHoverNext = () => {
+    setHoverNext(true);
+  };
+
+  const handleLeaveNext = () => {
+    setHoverNext(false);
+  };
+
+  const handleHoverPrev = () => {
+    setHoverPrev(true);
+  };
+
+  const handleLeavePrev = () => {
+    setHoverPrev(false);
+  };
+
   return (
     <Styled.SliderWrapper>
-      <Styled.SliderHeader>
-        <span>OUR WORK</span>
-        <span>OUR WORK</span>
-        <span>OUR WORK</span>
-        <span>OUR WORK</span>
-        <span>OUR WORK</span>
-        <span>OUR WORK</span>
-      </Styled.SliderHeader>
+      <Styled.NavigateLeft>
+        OUR WORK
+        <svg
+          width="15"
+          height="8"
+          viewBox="0 0 15 8"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M7.5 8L0.138784 0.5L14.8612 0.5L7.5 8Z" />
+        </svg>
+      </Styled.NavigateLeft>
       <Swiper {...SliderProps}>
         {currentProjects?.map((el, ind) => (
           <SwiperSlide key={ind}>
@@ -74,6 +92,64 @@ const ShowCase = ({ projects }: IShowCase) => {
           </SwiperSlide>
         ))}
       </Swiper>
+      <Styled.NavigateRight>
+        <Styled.ArrowContainer
+          onMouseOver={handleHoverPrev}
+          onMouseLeave={handleLeavePrev}
+          className={hoverPrev ? "hover" : undefined}
+        >
+          <svg
+            className="swiper-button-next"
+            fill="none"
+            width="100%"
+            height="100%"
+            preserveAspectRatio="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M23.2846 48.6355C32.2733 53.6972 43.6635 50.5591 48.7252 41.6264C53.787 32.6936 50.6035 21.3489 41.6147 16.2871C32.6259 11.2254 21.2358 14.3635 16.174 23.2962C11.1123 32.229 14.2958 43.5738 23.2846 48.6355Z"
+              fill="#F1EFED"
+              stroke="black"
+              strokeWidth="1.7"
+              strokeMiterlimit="10"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M31.9661 23.0272L41.2694 32.1009L41.258 32.1125L41.2697 32.1239L31.723 41.9058L30.3906 40.6058L37.7195 33.0964L23.7151 33.3079L23.6969 31.4417L37.6965 31.2303L30.6593 24.3668L31.9661 23.0272Z"
+              fill="black"
+            />
+          </svg>
+        </Styled.ArrowContainer>
+        <Styled.ArrowContainer
+          onMouseOver={handleHoverNext}
+          onMouseLeave={handleLeaveNext}
+          className={hoverNext ? "hover" : undefined}
+        >
+          <svg
+            className="swiper-button-prev"
+            fill="none"
+            width="100%"
+            height="100%"
+            preserveAspectRatio="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M41.5171 16.2198C32.498 11.2124 21.1269 14.4192 16.1192 23.3823C11.1115 32.3455 14.3634 43.6708 23.3825 48.6782C32.4017 53.6856 43.7727 50.4788 48.7804 41.5157C53.7882 32.5525 50.5363 21.2272 41.5171 16.2198Z"
+              fill="#F1EFED"
+              stroke="black"
+              strokeWidth="1.7"
+              strokeMiterlimit="10"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M32.9902 41.88L23.6322 32.8627L23.6435 32.8509L23.6318 32.8396L33.1193 23.0003L34.4595 24.2922L27.1761 31.8457L41.1789 31.5497L41.2084 33.4158L27.2103 33.7117L34.2889 40.5326L32.9902 41.88Z"
+              fill="black"
+            />
+          </svg>
+        </Styled.ArrowContainer>
+      </Styled.NavigateRight>
     </Styled.SliderWrapper>
   );
 };
