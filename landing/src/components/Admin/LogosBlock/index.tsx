eee210e2bdbef9330fc1102ee5cc6a09fd1f35eb
baliubakh <74013378+baliubakh@users.noreply.github.com>
2022-11-04 12:10:03 +0200

    fix: replaced admin drag and drop library

--- landing/src/components/Admin/LogosBlock/index.tsx
+++ landing/src/components/Admin/LogosBlock/index.tsx
@@ -1,21 +1,14 @@
 import { useFormikContext } from "formik";
 import React, { FC } from "react";
-import { SortableContainer, SortableElement } from "react-sortable-hoc";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { SortableItem } from "react-easy-sort";
+
 import { queryKeys } from "../../../consts/queryKeys";
 import { adminGlobalService } from "../../../services/adminHomePage";
 import * as Styled from "../../../styles/AdminPage";
 import { IDataResponse } from "../../../types/Admin/Response.types";
 import AddLogoFrame from "./AddLogoFrame";
 import LogoElement from "./LogoElement";
-
-interface IDragItem {
-  item: { url: string; index: number };
-}
-
-interface IDragContainer {
-  items: { url: string }[];
-}
 
 export interface IDragProps {
   oldIndex: number;
@@ -36,38 +29,7 @@ const AdminLogosBlock: FC = () => {
     queryClient.invalidateQueries([queryKeys.GetFullPage]);
   };
 
-  const SortableListItem = SortableElement<IDragItem>(({ item }: IDragItem) => {
-    return (
-      <div>
-        <LogoElement
-          image={item}
-          key={Math.random()}
-          deleteLogo={() => deleteNormalLogo(item.index)}
-        />
-      </div>
-    );
-  });
-
-  const SortableList = SortableContainer<IDragContainer>(
-    ({ items }: IDragContainer) => {
-      return (
-        <Styled.AdminLogosGrid>
-          <AddLogoFrame state={values.LogosBlock} submit={handleSubmit} />
-          {items.map((item, index) => {
-            return (
-              <SortableListItem
-                item={{ ...item, index }}
-                key={index}
-                index={index}
-              />
-            );
-          })}
-        </Styled.AdminLogosGrid>
-      );
-    }
-  );
-
-  const onSortEnd = ({ oldIndex, newIndex }: IDragProps) => {
+  const onSortEnd = (oldIndex: number, newIndex: number) => {
     values.LogosBlock.images.splice(
       newIndex,
       0,
@@ -79,12 +41,24 @@ const AdminLogosBlock: FC = () => {
   return (
     <div>
       <Styled.AdminSubTitle>Logos</Styled.AdminSubTitle>
-
-      <SortableList
-        axis="xy"
-        items={values.LogosBlock.images}
+      <Styled.AdminLogosGrid
         onSortEnd={onSortEnd}
-      />
+        className="list"
+        draggedItemClassName="dragged"
+      >
+        <AddLogoFrame state={values.LogosBlock} submit={handleSubmit} />
+        {values.LogosBlock.images.map((item, index) => (
+          <SortableItem key={item.url}>
+            <div>
+              <LogoElement
+                image={item}
+                key={Math.random()}
+                deleteLogo={() => deleteNormalLogo(index)}
+              />
+            </div>
+          </SortableItem>
+        ))}
+      </Styled.AdminLogosGrid>
     </div>
   );
 };
