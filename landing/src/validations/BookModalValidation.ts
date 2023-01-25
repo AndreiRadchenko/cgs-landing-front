d1167f2d7e8b797c9be804a64eabb8d93e7d4ad4
baliubakh <74013378+baliubakh@users.noreply.github.com>
2023-01-25 16:53:45 +0200

    fix: Changed error text for cooperation form

--- landing/src/validations/BookModalValidation.ts
+++ landing/src/validations/BookModalValidation.ts
@@ -3,7 +3,7 @@ import * as yup from "yup";
 export const BookModalValidation = () => {
   return yup.object({
     name: yup.string().required("How should we call you?"),
-    email: yup.string().email().required("Enter e-mail"),
-    service: yup.string().required("select service"),
+    email: yup.string().email().required("Enter email"),
+    service: yup.string().required("Select service"),
   });
 };
