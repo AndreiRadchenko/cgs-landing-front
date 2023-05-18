22d023caff9bcc722d1cb3000de115ccda629709
andreiradchenko <d4100kx@gmail.com>
2023-05-18 10:04:56 +0300

    feat: Implement mobile layout

--- landing/src/components/AboutUsPage/AboutUs.styled.ts
+++ landing/src/components/AboutUsPage/AboutUs.styled.ts
@@ -2,6 +2,199 @@ import styled from "styled-components";
 import themes from "../../utils/themes";
 import { infiniteText } from "../../styles/Animations.styled";
 
+export const HeroAboutContainer = styled.div`
+  width: 100vw - 8px;
+  height: 26.2vw;
+  position: relative;
+
+  @media ${themes.primary.media.maxTabletLandScape} {
+  }
+
+  @media ${themes.primary.media.maxTabletPortrait} {
+  }
+
+  @media ${themes.primary.media.maxLowScreenMobile} {
+  }
+  @media ${themes.primary.media.maxMobile} {
+    height: 198px;
+  }
+`;
+
+export const HeadlinesContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 44px;
+  gap: 66px;
+  @media ${themes.primary.media.maxMobile} {
+    display: block;
+    margin-top: 0;
+  }
+`;
+
+export const HeadlineContainer = styled.div`
+  @media ${themes.primary.media.maxMobile} {
+    margin-bottom: 90px;
+  }
+`;
+
+export const NumbersContainer = styled.div`
+  box-sizing: border-box;
+  margin-top: 120px;
+  height: 174px;
+  width: 100vw;
+  margin-left: -4.25em;
+  background-color: rgba(143, 142, 147, 0.15);
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
+
+  @media ${themes.primary.media.maxMobile} {
+    margin-top: 90px;
+    display: grid;
+    height: 280px;
+    margin-left: -2.65em;
+    padding: 28px 20px;
+    grid-template-columns: 1fr 1fr;
+    & > :nth-child(1) {
+      grid-row: 2;
+      grid-column: 1;
+    }
+    & > :nth-child(2) {
+      grid-row: 1;
+      grid-column: 1;
+    }
+    & > :nth-child(3) {
+      grid-row: 1;
+      grid-column: 2;
+    }
+    & > :nth-child(4) {
+      grid-row: 2;
+      grid-column: 2;
+    }
+  }
+`;
+
+export const Achievement = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0;
+`;
+
+export const AchievementNumber = styled.p`
+  font-weight: 900;
+  font-size: 80px;
+  line-height: 90px;
+  margin: 0;
+  letter-spacing: 0.05em;
+  color: #f1efed;
+  text-shadow: 7px 2px 0px #000000, -1px 0px 0px #000000, 0px 1px 0px #000000,
+    0px -1px 0px #000000, 1px 0px 0px #000000;
+  @media ${themes.primary.media.maxMobile} {
+    font-size: 60px;
+  }
+`;
+
+export const AchievementText = styled.p`
+  font-weight: 900;
+  font-size: 18px;
+  line-height: 22px;
+  margin: 4px 0 0;
+  @media ${themes.primary.media.maxMobile} {
+    font-size: 14px;
+    line-height: 16px;
+  }
+`;
+
+export const OurTeamContainer = styled.div`
+  margin: 120px 0;
+  @media ${themes.primary.media.maxMobile} {
+    margin-top: 90px;
+    margin-bottom: 78px;
+  }
+`;
+
+export const TeamGallery = styled.ul`
+  list-style: none;
+  margin: 56px 0 0;
+  padding: 0;
+  display: flex;
+  justify-content: space-between;
+  gap: 50px;
+  @media ${themes.primary.media.maxTabletLandScape} {
+    gap: 20px;
+  }
+  @media ${themes.primary.media.maxMobile} {
+    flex-direction: column;
+    align-items: center;
+    gap: 32px;
+    margin-top: 0;
+  }
+`;
+
+export const Teammate = styled.li`
+  width: 400px;
+  display: flex;
+  flex-direction: column;
+  @media ${themes.primary.media.maxServiceWeb} {
+    width: 30%;
+  }
+  @media ${themes.primary.media.maxMobile} {
+    width: 332px;
+  }
+`;
+
+export const TeammateImageContainer = styled.div`
+  height: 380px;
+  position: relative;
+  @media ${themes.primary.media.maxServiceWeb} {
+    width: 332px;
+    height: 318px;
+  }
+  @media ${themes.primary.media.maxTabletLandScape} {
+    width: 100%;
+    height: 25vw;
+  }
+  @media ${themes.primary.media.maxMobile} {
+    width: 332px;
+    height: 318px;
+  }
+`;
+
+export const LinkedIn = styled.a`
+  position: absolute;
+  top: 18px;
+  right: 18px;
+`;
+
+export const TeammateName = styled.p`
+  margin: 18px 0 0;
+  font-size: 24px;
+  line-height: 160%;
+  text-transform: uppercase;
+`;
+
+export const TeammatePosition = styled.p`
+  margin: 0;
+  font-size: 24px;
+  line-height: 160%;
+  text-transform: uppercase;
+  color: #8f8e93;
+`;
+
+export const TeammateAbout = styled.p`
+  margin: 18px 0 0;
+  line-height: 160%;
+  font-size: ${themes.primary.font.size.aboutUsBelieveText};
+  @media ${themes.primary.media.maxTabletLandScape} {
+    font-size: 1em;
+  }
+  @media ${themes.primary.media.maxMobile} {
+    font-size: 16px;
+  }
+`;
+
 export const DescriptionContainer = styled.section`
   margin-bottom: 47px;
   @media ${themes.primary.media.maxTabletLandScape} {
@@ -49,6 +242,7 @@ export const Subtitle = styled.div`
   font-size: ${themes.primary.font.size.aboutUsSubtitle};
   font-weight: ${themes.primary.font.weight.heavy};
   margin-bottom: 20px;
+  text-transform: uppercase;
   @media ${themes.primary.media.maxTabletLandScape} {
     font-size: ${themes.primary.font.size.smallAboutSubtitle};
   }
