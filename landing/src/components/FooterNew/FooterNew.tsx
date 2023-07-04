fc15401f28828b960341492ffbf2c4fad2feddb3
Fembulvetr <113340525+Fembulvetr@users.noreply.github.com>
2023-07-04 13:51:05 +0300

    fix: Footer bugs and change animation

--- landing/src/components/FooterNew/FooterNew.tsx
+++ landing/src/components/FooterNew/FooterNew.tsx
@@ -1,5 +1,6 @@
 import React, {
   Dispatch,
+  MutableRefObject,
   SetStateAction,
   useEffect,
   useRef,
@@ -14,7 +15,6 @@ import { IDataResponse } from "../../types/Admin/Response.types";
 import { queryKeys } from "../../consts/queryKeys";
 import Image from "next/image";
 import smallMountain from "/public/smallMountain.svg";
-import { useOnScreen } from "../../hooks/useOnScreen";
 import Logo from "../HeaderNavNew/Logo";
 import ExplanationEmailField from "./ExplanationEmailField/ExplanationEmailField";
 
@@ -24,6 +24,28 @@ interface IFooterProps {
   clickFromEstimationForm?: boolean;
 }
 
+const useInViewport = <T extends Element>(ref: MutableRefObject<T | null>) => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (ref.current) {
+        const { top, bottom } = ref.current.getBoundingClientRect();
+        const isInViewport = top <= window.innerHeight && bottom >= 0;
+        setIsScrolled(isInViewport);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [ref]);
+
+  return isScrolled;
+};
+
 const FooterNew = ({
   className,
   setOpenFailedModal,
@@ -31,10 +53,24 @@ const FooterNew = ({
 }: IFooterProps): JSX.Element => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const { width } = useWindowDimension();
+  const [animationPlay, setAnimationPlay] = useState(false);
 
   const elRef = useRef<HTMLDivElement>(null);
 
-  const isScrolled = useOnScreen(elRef, true);
+  const isScrolled = useInViewport(elRef);
+
+  useEffect(() => {
+    if (isScrolled) {
+      setAnimationPlay(true);
+      const interval = setInterval(() => {
+        setAnimationPlay((prevAnimationPlay) => !prevAnimationPlay);
+      }, 5000);
+
+      return () => clearInterval(interval);
+    } else {
+      setAnimationPlay(false)
+    }
+  }, [isScrolled]);
 
   useEffect(() => {
     isOpen && width && width >= 768 && setIsOpen(false);
@@ -69,7 +105,7 @@ const FooterNew = ({
                 viewBox="0 0 27 19"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
-                className={isScrolled ? "scrolled" : undefined}
+                className={animationPlay ? "scrolled" : undefined}
               >
                 <path d="M1.90909 2.88889H3.72727M3.72727 4.77778H5.54545M5.54545 6.66667H7.36364M7.36364 8.55556H9.18182M9.18182 10.4444H11M25.0909 2.88889H23.2727M23.2727 4.77778H21.4545M21.4545 6.66667H19.6364M19.6364 8.55556H17.8182M11 12.3333H12.8182H14.1818H16M17.8182 10.4444H16M1 1V18H26V1H1Z" />
               </StyledThisComp.MailIcon>
@@ -87,7 +123,7 @@ const FooterNew = ({
                 viewBox="0 0 27 19"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
-                className={isScrolled ? "scrolled" : undefined}
+                className={animationPlay ? "scrolled" : undefined}
               >
                 <path d="M1.90909 2.88889H3.72727M3.72727 4.77778H5.54545M5.54545 6.66667H7.36364M7.36364 8.55556H9.18182M9.18182 10.4444H11M25.0909 2.88889H23.2727M23.2727 4.77778H21.4545M21.4545 6.66667H19.6364M19.6364 8.55556H17.8182M11 12.3333H12.8182H14.1818H16M17.8182 10.4444H16M1 1V18H26V1H1Z" />
               </StyledThisComp.MailIcon>
