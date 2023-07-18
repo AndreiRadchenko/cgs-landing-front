018e9f459f2a34a3cdf833a60f27459798af8e8c
Ruslan Miahkykh <elitkondor@gmail.com>
2023-07-18 20:00:42 +0300

    fix: Rewrite a function

--- landing/src/utils/onBlurHandler.ts
+++ landing/src/utils/onBlurHandler.ts
@@ -4,11 +4,11 @@ export const onBlurHandler = (
   ref: React.MutableRefObject<any>,
   callback: () => void
 ) => {
-  function handleClickOutside(event: { target: any }) {
+  const handleClickOutside = (event: { target: any }) => {
     if (ref.current && !ref.current.contains(event.target)) {
       callback();
     }
-  }
+  };
 
   useEffect(() => {
     document.addEventListener("mousedown", handleClickOutside);
