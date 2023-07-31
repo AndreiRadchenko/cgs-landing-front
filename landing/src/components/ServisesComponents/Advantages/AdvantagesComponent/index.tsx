d6baafc47860007a5de312bcc31eb59898b87384
Fembulvetr <113340525+Fembulvetr@users.noreply.github.com>
2023-07-31 11:15:19 +0300

    fix: Advantages adaptive on mobile

--- landing/src/components/ServisesComponents/Advantages/AdvantagesComponent/index.tsx
+++ landing/src/components/ServisesComponents/Advantages/AdvantagesComponent/index.tsx
@@ -8,6 +8,7 @@ import { useOnScreen } from "../../../../hooks/useOnScreen";
 
 import circle from "../../../../../public/Advantages/circle.svg";
 import smallCircle from "../../../../../public/Advantages/smallCircle.svg";
+import { useMediaQuery } from "@mui/material";
 
 interface IAdvantagesProps {
   advantages?: IAdvantagesServicesComponent;
@@ -17,6 +18,7 @@ interface IAdvantagesProps {
 const Advantages = ({ advantages, className }: IAdvantagesProps) => {
   const elRef = useRef<HTMLDivElement>(null);
   const isScrolled = useOnScreen(elRef, true);
+  const isMobile = useMediaQuery("(max-width: 768px)")
 
   const advantagesCount =
     advantages?.content?.filter(
@@ -28,59 +30,88 @@ const Advantages = ({ advantages, className }: IAdvantagesProps) => {
     return number;
   });
 
+  const combinedArray = numbers.map((number, idx) => ({
+    number,
+    ...advantages?.content[idx],
+  }));
+
   return (
     <Styled.Wrapper className={className}>
       <Styled.Title>{advantages?.title}</Styled.Title>
-      <Styled.Content>
-        <Styled.Numbers>
-          {numbers.map((number, idx) => (
-            <Styled.NumberItems
-              key={idx}
-              ind={idx}
-              className={isScrolled ? "scrolled" : undefined}
-            >
-              <p>{number}</p>
-              <Styled.ImageWrapper>
-                {idx === numbers.length - 1 ? (
-                  <>
-                    <Styled.BlockContainer>
-                      <img src={circle.src} alt="Yellow Circle" />
-                      <Styled.AfterBlock />
-                      <img src={smallCircle.src} alt="Dot" />
-                    </Styled.BlockContainer>
-                    <Styled.BeforeBlock isFirst={true} />
-                  </>
-                ) : (
-                  <>
-                    <Styled.BlockContainer>
-                      <img src={circle.src} alt="Yellow Circle" />
-                      <Styled.AfterBlock />
-                      <img src={smallCircle.src} alt="Dot" />
-                      <Styled.BeforeBlock isFirst={false} />
-                    </Styled.BlockContainer>
-                  </>
-                )}
-              </Styled.ImageWrapper>
-            </Styled.NumberItems>
+      {isMobile ?
+        <Styled.MobileContent>
+          {combinedArray.map((item, idx) => (
+            <Styled.AdvantagesItem key={idx}>
+              <Styled.NumberMobileItem>
+                <Styled.MobileNumber className={idx === combinedArray.length - 1 ? "last" : ''}>{item.number}</Styled.MobileNumber>
+                <Styled.Circle
+                  src={circle.src}
+                  alt="Yellow Circle"
+                />
+                <Styled.SmallCircle
+                  src={smallCircle.src}
+                  alt="Dot"
+                />
+              </Styled.NumberMobileItem>
+              <Styled.TextMobileItem ind={idx} className={isScrolled ? "scrolled" : undefined}>
+                <Styled.Subtitle>{item.subtitle}</Styled.Subtitle>
+                <Styled.Text>{item.text}</Styled.Text>
+              </Styled.TextMobileItem>
+            </Styled.AdvantagesItem>
           ))}
-        </Styled.Numbers>
-        <Styled.TextContent ref={elRef}>
-          {advantages?.content
-            ?.filter(
-              (item) => item.subtitle.trim() !== "" && item.text.trim() !== ""
-            )
-            .map(({ subtitle, text }, idx) => (
-              <Styled.ContentItems
+        </Styled.MobileContent>
+        :
+        <Styled.Content>
+          <Styled.Numbers>
+            {numbers.map((number, idx) => (
+              <Styled.NumberItems
                 key={idx}
                 ind={idx}
                 className={isScrolled ? "scrolled" : undefined}
               >
-                <Styled.Subtitle>{subtitle}</Styled.Subtitle>
-                <Styled.Text>{text}</Styled.Text>
-              </Styled.ContentItems>
+                <p>{number}</p>
+                <Styled.ImageWrapper>
+                  {idx === numbers.length - 1 ? (
+                    <>
+                      <Styled.BlockContainer>
+                        <img src={circle.src} alt="Yellow Circle" />
+                        <Styled.AfterBlock />
+                        <img src={smallCircle.src} alt="Dot" />
+                      </Styled.BlockContainer>
+                      <Styled.BeforeBlock isFirst={true} />
+                    </>
+                  ) : (
+                    <>
+                      <Styled.BlockContainer>
+                        <img src={circle.src} alt="Yellow Circle" />
+                        <Styled.AfterBlock />
+                        <img src={smallCircle.src} alt="Dot" />
+                        <Styled.BeforeBlock isFirst={false} />
+                      </Styled.BlockContainer>
+                    </>
+                  )}
+                </Styled.ImageWrapper>
+              </Styled.NumberItems>
             ))}
-        </Styled.TextContent>
-      </Styled.Content>
+          </Styled.Numbers>
+          <Styled.TextContent ref={elRef}>
+            {advantages?.content
+              ?.filter(
+                (item) => item.subtitle.trim() !== "" && item.text.trim() !== ""
+              )
+              .map(({ subtitle, text }, idx) => (
+                <Styled.ContentItems
+                  key={idx}
+                  ind={idx}
+                  className={isScrolled ? "scrolled" : undefined}
+                >
+                  <Styled.Subtitle>{subtitle}</Styled.Subtitle>
+                  <Styled.Text>{text}</Styled.Text>
+                </Styled.ContentItems>
+              ))}
+          </Styled.TextContent>
+        </Styled.Content>
+      }
     </Styled.Wrapper>
   );
 };
