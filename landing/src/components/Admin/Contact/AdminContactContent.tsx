f77823a3065b839b7d4d50747fe3cfb99da9832a
andreiradchenko <d4100kx@gmail.com>
2023-08-15 00:33:55 +0300

    feat: Add notification to the main page and contact page

--- landing/src/components/Admin/Contact/AdminContactContent.tsx
+++ landing/src/components/Admin/Contact/AdminContactContent.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Formik } from "formik";
 import { useMutation, useQuery } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import { queryKeys } from "../../../consts/queryKeys";
 import { adminContactService } from "../../../services/adminContactPage";
 import ContactForm from "./ContactForm";
+import { CustomToast } from "../CustomToast";
 
 import * as Styled from "../../../styles/AdminPage";
 import {
@@ -20,7 +23,17 @@ const AdminContactContent = () => {
 
   const { mutateAsync: updateContactPage } = useMutation(
     [queryKeys.updateContactPage],
-    (data: IContactPageData) => adminContactService.updateContactPage(data)
+    async (data: IContactPageData) => {
+      const response = await toast.promise(
+        adminContactService.updateContactPage(data),
+        {
+          pending: "Pending update",
+          success: "Data updated successfully 👌",
+          error: "Some things went wrong 🤯",
+        }
+      );
+      return response;
+    }
   );
 
   const submitForm = async (values: IContactPageData) => {
@@ -33,9 +46,12 @@ const AdminContactContent = () => {
   return isLoading ? (
     <Styled.AdminUnauthorizedModal>Loading...</Styled.AdminUnauthorizedModal>
   ) : data !== undefined ? (
-    <Formik initialValues={data} onSubmit={submitForm}>
-      <ContactForm />
-    </Formik>
+    <>
+      <Formik initialValues={data} onSubmit={submitForm}>
+        <ContactForm />
+      </Formik>
+      <CustomToast />
+    </>
   ) : (
     <Styled.AdminUnauthorizedModal>
       Something went wrong :(
