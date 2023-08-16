307463816afbc1a16babafea0674e5108d9a520e
andreiradchenko <d4100kx@gmail.com>
2023-08-16 15:10:11 +0300

    fix: Set coop form phone field non required

--- landing/src/utils/checkEmailDomain.ts
+++ landing/src/utils/checkEmailDomain.ts
@@ -1,4 +1,5 @@
-const publickDomainList = ["gmail", "yahoo", "outlook", "icloud"];
+// const publickDomainList = ["gmail", "yahoo", "outlook", "icloud"];
+const publickDomainList: string[] = [];
 
 export const isEmailDomainPublic = (email: string) => {
   return publickDomainList.some((e) => email?.toLowerCase().includes(`@${e}.`));
