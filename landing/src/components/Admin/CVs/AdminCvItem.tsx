b44ff88633ce614e5cc1f03756221f7adddf18cc
Fembulvetr <113340525+Fembulvetr@users.noreply.github.com>
2023-08-16 14:28:34 +0300

    feat: Save pdf first test

--- landing/src/components/Admin/CVs/AdminCvItem.tsx
+++ landing/src/components/Admin/CVs/AdminCvItem.tsx
@@ -1,6 +1,8 @@
 import { useQueryClient } from "@tanstack/react-query";
 import React from "react";
 import Image from "next/image";
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 import { queryKeys } from "../../../consts/queryKeys";
 import ButtonArrow from "../../../utils/ButtonArrow";
@@ -44,44 +46,68 @@ const AdminCvItem = ({
         }
         if (editFlag) onScroll();
     };
+
+
+    const generatePDF = async (id: string) => {
+        const input = document.getElementById(id);
+        if (!input) return;
+
+        // const canvas = await html2canvas(input);
+        // const imgData = canvas.toDataURL('image/png');
+
+        const canvas = await html2canvas(input, {
+            backgroundColor: "none",
+            logging: true,
+            useCORS: true
+        });
+
+        const data = canvas.toDataURL("image/png");
+
+        const pdf = new jsPDF();
+        const width = pdf.internal.pageSize.getWidth();
+        const height = (canvas.height * width) / canvas.width;
+        pdf.addImage(data, 'PNG', 0, 0, width, height);
+        pdf.save('cv.pdf');
+    };
+
     return (
         <>
-        <Styles.AdminCvItemFrame className={editFlag ? undefined : !editFlag && current !== idx ? "fade" : undefined}>
-            <Styles.AdminCvItemFlexContent>
-                <Styles.AdminCvItemLeftFlex>
-                    <img
-                        src={cv.image.url}
-                        width={155}
-                        height={155}
-                        alt="admin cv image"
-                    />
-                    <Styles.AdminCvItemTitle>
-                        <Styles.AdminCvItemName>{cv.personal.name}</Styles.AdminCvItemName>
-                        <Styles.AdminCvItemRole>{cv.personal.role}</Styles.AdminCvItemRole>
-                    </Styles.AdminCvItemTitle>
-                </Styles.AdminCvItemLeftFlex>
-                <Styles.AdminCvItemRightFlex>
-                    <Styles.AdminCvItemLink>
-                        <a href={`/cv/${cv.personal.name.toLowerCase().replace(/\s+/g, '-')}-${cv._id}`} target="_blank">link</a>
-                        <ArrowContainer>
-                            <ButtonArrow />
-                        </ArrowContainer>
-                    </Styles.AdminCvItemLink>
-                    <Styles.AdminCvItemExport>export as PDF</Styles.AdminCvItemExport>
-                </Styles.AdminCvItemRightFlex>
-            </Styles.AdminCvItemFlexContent>
-            <Styles.AdminCvItemEditDelete>
-                <Styles.AdminCvItemEdit onClick={editTriggerFunc}>
-                    <Image
-                        src={editFlag || current !== idx ? edit : close}
-                        alt="admin cv edit icon"
-                    />
-                </Styles.AdminCvItemEdit>
-                <Styles.AdminCvItemDelete onClick={deleteFunc}>
-                    delete
-                </Styles.AdminCvItemDelete>
-            </Styles.AdminCvItemEditDelete>
-        </Styles.AdminCvItemFrame>
+            <Styles.AdminCvItemFrame className={editFlag ? undefined : !editFlag && current !== idx ? "fade" : undefined}>
+                <Styles.AdminCvItemFlexContent>
+                    <Styles.AdminCvItemLeftFlex id={cv._id}>
+                        <img
+                            src={cv.image.url}
+                            width={155}
+                            height={155}
+                            alt="admin cv image"
+                        />
+                        <Styles.AdminCvItemTitle>
+                            <Styles.AdminCvItemName>{cv.personal.name}</Styles.AdminCvItemName>
+                            <Styles.AdminCvItemRole>{cv.personal.role}</Styles.AdminCvItemRole>
+                        </Styles.AdminCvItemTitle>
+                    </Styles.AdminCvItemLeftFlex>
+                    <Styles.AdminCvItemRightFlex>
+                        <Styles.AdminCvItemLink>
+                            <a href={`/cv/${cv.personal.name.toLowerCase().replace(/\s+/g, '-')}-${cv._id}`} target="_blank">link</a>
+                            <ArrowContainer>
+                                <ButtonArrow />
+                            </ArrowContainer>
+                        </Styles.AdminCvItemLink>
+                        <Styles.AdminCvItemExport onClick={() => generatePDF(cv._id)}>export as PDF</Styles.AdminCvItemExport>
+                    </Styles.AdminCvItemRightFlex>
+                </Styles.AdminCvItemFlexContent>
+                <Styles.AdminCvItemEditDelete>
+                    <Styles.AdminCvItemEdit onClick={editTriggerFunc}>
+                        <Image
+                            src={editFlag || current !== idx ? edit : close}
+                            alt="admin cv edit icon"
+                        />
+                    </Styles.AdminCvItemEdit>
+                    <Styles.AdminCvItemDelete onClick={deleteFunc}>
+                        delete
+                    </Styles.AdminCvItemDelete>
+                </Styles.AdminCvItemEditDelete>
+            </Styles.AdminCvItemFrame>
         </>
     )
 }
